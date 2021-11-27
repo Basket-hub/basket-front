2b3abcccb10705e7980d26b23f5f024dd209fb96
Andri <anluik@ttu.ee>
2021-11-27 10:53:56 +0200

    styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,14 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent implements OnInit {
   public faSearch = faSearch;
+  public faSpinner = faSpinner;
+
+  public someRandomItems = [
+    { id: 1, name: "cheese", price: 4.49 },
+    { id: 2, name: "yogurt", price: 1.24 },
+    { id: 3, name: "bread", price: 0.65 },
+    { id: 4, name: "milk", price: 1.12 },
+  ]
 
   constructor() { }
 
